4b5270561d27ac7496000a9c2837b1edd5af11a1
longBright <longbright0804@gmail.com>
2023-04-29 20:37:14 +0900

    SF-39/fix: 도메인 수정

--- src/main/java/com/ssafy/faraway/domain/post/entity/Post.java
+++ src/main/java/com/ssafy/faraway/domain/post/entity/Post.java
@@ -4,12 +4,9 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Builder
 public class Post {
     private long id;
     private long memberId;
@@ -19,4 +16,12 @@ public class Post {
     private long hit;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
+
+    @Builder
+    public Post(long memberId, String category, String title, String content) {
+        this.memberId = memberId;
+        this.category = category;
+        this.title = title;
+        this.content = content;
+    }
 }
