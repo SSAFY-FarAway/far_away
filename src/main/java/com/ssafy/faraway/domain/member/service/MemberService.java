6c7a3845cdfd32c171c63d23f0554794fb2991c0
wjs_5025 <wjs_5025@naver.com>
2023-04-29 23:40:32 +0900

    SF-51/feat : Service 및 Mapper 구현

--- src/main/java/com/ssafy/faraway/domain/member/service/MemberService.java
+++ src/main/java/com/ssafy/faraway/domain/member/service/MemberService.java
@@ -1,7 +1,20 @@
 package com.ssafy.faraway.domain.member.service;
 
 import com.ssafy.faraway.domain.member.dto.req.MemberSaveRequestDto;
+import com.ssafy.faraway.domain.member.dto.req.MemberUpdateRequestDto;
+import com.ssafy.faraway.domain.member.dto.res.MemberListResponseDto;
+import com.ssafy.faraway.domain.member.dto.res.MemberResponseDto;
+
+import java.sql.SQLException;
 
 public interface MemberService {
-    Integer save(MemberSaveRequestDto memberSaveRequestDto);
+    Integer save(MemberSaveRequestDto memberSaveRequestDto) throws SQLException;
+
+    MemberResponseDto find(String id) throws SQLException;
+
+    MemberListResponseDto findAll() throws SQLException;
+
+    Integer update(MemberUpdateRequestDto memberUpdateRequestDto) throws SQLException;
+
+    Integer delete(String id) throws SQLException;
 }
--- src/main/java/com/ssafy/faraway/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/ssafy/faraway/domain/member/service/MemberServiceImpl.java
@@ -1,19 +1,44 @@
 package com.ssafy.faraway.domain.member.service;
 
 import com.ssafy.faraway.domain.member.dto.req.MemberSaveRequestDto;
+import com.ssafy.faraway.domain.member.dto.req.MemberUpdateRequestDto;
+import com.ssafy.faraway.domain.member.dto.res.MemberListResponseDto;
+import com.ssafy.faraway.domain.member.dto.res.MemberResponseDto;
 import com.ssafy.faraway.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
+
 @RequiredArgsConstructor
 @Service
-public class MemberServiceImpl {
+public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
     public Integer save(MemberSaveRequestDto memberSaveRequestDto){
         return memberRepository.save(memberSaveRequestDto);
+    }
+
+    @Override
+    public MemberResponseDto find(String id) throws SQLException {
+        return memberRepository.find(id);
+    }
+
+    @Override
+    public MemberListResponseDto findAll() throws SQLException {
+        return memberRepository.findAll();
+    }
+
+    @Override
+    public Integer update(MemberUpdateRequestDto memberUpdateRequestDto) throws SQLException {
+        return memberRepository.update(memberUpdateRequestDto);
+    }
+
+    @Override
+    public Integer delete(String id) throws SQLException {
+        return memberRepository.delete(id);
     }
 
 }
