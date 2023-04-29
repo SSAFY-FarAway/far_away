bdae7c14eeeee34adca8b17870e4261be36df1d2
yoojin Jang <yoojinjangjang@gmail.com>
2023-04-29 19:21:49 +0900

    swagger init

--- src/main/java/com/ssafy/faraway/domain/attraction/controller/AttractionController.java
+++ src/main/java/com/ssafy/faraway/domain/attraction/controller/AttractionController.java
@@ -1,4 +1,18 @@
 package com.ssafy.faraway.domain.attraction.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attraction")
+@Api(tags="attraction")
 public class AttractionController {
+
+    @RequestMapping("/")
+    public ResponseEntity<Integer> test() {
+        return new ResponseEntity<>(0, HttpStatus.OK);
+    }
 }
