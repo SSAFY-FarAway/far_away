b402ca045279e9a94524a0a51c456305d811004f
wjs_5025 <wjs_5025@naver.com>
2023-04-30 00:31:41 +0900

    SF-51/feat : 컨트롤러 구현

--- src/main/java/com/ssafy/faraway/domain/member/controller/MemberController.java
+++ src/main/java/com/ssafy/faraway/domain/member/controller/MemberController.java
@@ -1,8 +1,56 @@
 package com.ssafy.faraway.domain.member.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.faraway.domain.member.dto.req.MemberSaveRequestDto;
+import com.ssafy.faraway.domain.member.dto.res.MemberListResponseDto;
+import com.ssafy.faraway.domain.member.service.MemberService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+@Api(tags = "member")
 public class MemberController {
+    private final MemberService memberService;
 
+    @GetMapping("/")
+    public ResponseEntity<List<MemberListResponseDto>> findAll() {
+        System.out.println("gd");
+        List<MemberListResponseDto> list = null;
+        try {
+            list = memberService.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (list != null && !list.isEmpty()) {
+            return new ResponseEntity<List<MemberListResponseDto>>(list, HttpStatus.OK);
+        }
+        return new ResponseEntity<List<MemberListResponseDto>>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> save(@RequestBody MemberSaveRequestDto memberSaveRequestDto) {
+        try {
+            memberService.save(memberSaveRequestDto);
+            List<MemberListResponseDto> list = memberService.findAll();
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return exceptionHandling(e);
+        }
+    }
+
+
+
+    private ResponseEntity<String> exceptionHandling(Exception e) {
+        e.printStackTrace();
+        return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
