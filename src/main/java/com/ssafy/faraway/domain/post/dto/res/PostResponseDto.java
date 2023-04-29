c168bd9337ee71e72fcb58dfbcb09affc5276c27
longBright <longbright0804@gmail.com>
2023-04-29 20:57:41 +0900

    SF-39/fix: DTO 클래스 수정

--- src/main/java/com/ssafy/faraway/domain/post/dto/res/PostResponseDto.java
+++ src/main/java/com/ssafy/faraway/domain/post/dto/res/PostResponseDto.java
@@ -2,26 +2,19 @@ package com.ssafy.faraway.domain.post.dto.res;
 
 import com.ssafy.faraway.domain.post.entity.Post;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 public class PostResponseDto {
     private long id;
     private long memberId;
+    private String memberName;
     private String category;
     private String title;
     private String content;
     private long hit;
     private LocalDateTime modifiedDate;
-
-    public PostResponseDto(Post entity) {
-        this.id = entity.getId();
-        this.memberId = entity.getMemberId();
-        this.category = entity.getCategory();
-        this.title = entity.getTitle();
-        this.content = entity.getContent();
-        this.hit = entity.getHit();
-        this.modifiedDate = entity.getModifiedDate();
-    }
 }
