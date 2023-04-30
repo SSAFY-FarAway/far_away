e423d26f3255945f4ca399309890031a1a7be4c1
jinsikhong <eat159357@kookmin.ac.kr>
2023-04-30 14:24:18 +0900

    SF-21/member: 기능 구현 merge

--- src/main/java/com/ssafy/faraway/domain/member/controller/MemberController.java
+++ src/main/java/com/ssafy/faraway/domain/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ssafy.faraway.domain.member.controller;
 import com.ssafy.faraway.domain.member.dto.req.MemberSaveRequestDto;
 import com.ssafy.faraway.domain.member.dto.req.MemberUpdateRequestDto;
 import com.ssafy.faraway.domain.member.dto.res.MemberListResponseDto;
+import com.ssafy.faraway.domain.member.dto.res.MemberResponseDto;
 import com.ssafy.faraway.domain.member.service.MemberService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -25,7 +25,7 @@ public class MemberController {
         try {
             MemberResponseDto memberResponseDto = memberService.find(id);
             if (memberResponseDto != null)
-                return new ResponseEntity<MemberResponseDto>(memberResponseDto, HttpStatus.OK);
+                return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
             else
                 return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
