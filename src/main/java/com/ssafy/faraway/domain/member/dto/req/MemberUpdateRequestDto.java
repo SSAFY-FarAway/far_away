731bcf93ff6aea4139777344fa537d9c9e6e38a7
wjs_5025 <wjs_5025@naver.com>
2023-04-30 18:44:27 +0900

    SF-84/feat : DTO 유효성 검증

--- src/main/java/com/ssafy/faraway/domain/member/dto/req/MemberUpdateRequestDto.java
+++ src/main/java/com/ssafy/faraway/domain/member/dto/req/MemberUpdateRequestDto.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class MemberUpdateRequestDto {
