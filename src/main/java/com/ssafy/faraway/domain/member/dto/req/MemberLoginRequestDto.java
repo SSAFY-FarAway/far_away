5a0f168fa0d3a4d30d4b13b434dac0f0cc6655f7
wjs_5025 <wjs_5025@naver.com>
2023-04-30 15:26:22 +0900

    SF-80/feat : Login DTO 클래스 생성

--- src/main/java/com/ssafy/faraway/domain/member/dto/req/MemberLoginRequestDto.java
+++ src/main/java/com/ssafy/faraway/domain/member/dto/req/MemberLoginRequestDto.java
@@ -1,2 +1,34 @@
-package com.ssafy.faraway.domain.member.dto.req;public class MemberLoginRequestDto {
+package com.ssafy.faraway.domain.member.dto.req;
+
+import com.ssafy.faraway.domain.member.entity.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberLoginRequestDto {
+    private String loginId;
+    private String loginPwd;
+    private String salt;
+
+    @Builder
+    public MemberLoginRequestDto(String loginId, String loginPwd, String salt) {
+        this.loginId = loginId;
+        this.loginPwd = loginPwd;
+        this.salt = salt;
+    }
+
+    public void setLoginPwd(String loginPwd) {
+        this.loginPwd = loginPwd;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .loginId(loginId)
+                .loginPwd(loginPwd)
+                .salt(salt)
+                .build();
+    }
 }
+
