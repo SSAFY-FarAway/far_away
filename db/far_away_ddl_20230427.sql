b8599557ef4a4e38877610f41f611ee14ff30891
yoojinjangjang <yoojinjangjang@gmail.com>
2023-05-01 13:07:45 +0900

    db modify

--- db/far_away_ddl_20230427.sql
+++ db/far_away_ddl_20230427.sql
@@ -47,7 +47,6 @@ CREATE TABLE `member` (
                           `certified` int NOT NULL DEFAULT 0 COMMENT  'Integer',
                           `created_date`	timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP	COMMENT 'LocalDateTime',
                           `modified_date`	timestamp  NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP	COMMENT 'LocalDateTime'
-
 );
 
 DROP TABLE IF EXISTS `hotplace`;
