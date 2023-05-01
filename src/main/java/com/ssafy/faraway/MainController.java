f9a5e50f8cd1b08b30fb12f92bf5124c09f680bc
wjs5025 <wjs_5025@naver.com>
2023-05-01 15:56:36 +0900

    SF-111/feat : 로그인 view 구현

--- src/main/java/com/ssafy/faraway/MainController.java
+++ src/main/java/com/ssafy/faraway/MainController.java
@@ -11,4 +11,9 @@ public class MainController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/member/login")
+    public String mvLogin () {
+        return "member/login";
+    }
 }
