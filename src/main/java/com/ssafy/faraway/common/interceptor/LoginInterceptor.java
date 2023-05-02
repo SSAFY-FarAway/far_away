0c0fcdeaf07c4634187bbe905035cf78162876f4
longBright <longbright0804@gmail.com>
2023-05-02 12:10:09 +0900

    SF-110/fix: session ID 권한검사 처리

--- src/main/java/com/ssafy/faraway/common/interceptor/LoginInterceptor.java
+++ src/main/java/com/ssafy/faraway/common/interceptor/LoginInterceptor.java
@@ -13,7 +13,15 @@ import java.io.IOException;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String requestMethod = request.getMethod();
         HttpSession session = request.getSession(false);
+
+        // member 제외 모든 GET 요청 허용
+        if (!requestURI.contains("member") && requestMethod.equals("GET")) {
+            return true;
+        }
+
         if (session == null || session.getAttribute("loginMember") == null) {
             if (isAjaxRequest(request)) {
                 try {
